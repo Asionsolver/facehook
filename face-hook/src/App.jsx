5ff1c9e79ea169ec2c136943c0fea8f3d35990ec
asionsolver <asionsolver274@gmail.com>
2024-03-13 20:37:23 +0600

    Project Setup and Routing complete in FaceHook

--- face-hook/src/App.jsx
+++ face-hook/src/App.jsx
@@ -1,12 +1,22 @@
-
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import LogIn from "./pages/LogIn"
+import NotFound from "./pages/NotFound"
+import Profile from "./pages/Profile"
+import Registration from "./pages/Registration"
 
 function App() {
  
   return (
     <>
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <Routes>
+        <Route path="/" element={<Home />} exact/>
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/register" element={<Registration />} />
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   )
 }
